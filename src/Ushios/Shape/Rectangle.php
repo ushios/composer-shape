73c0dcacd47efc31f782d9a0e0c6ba5d67557395
UshioShugo <ushio.s@gmail.com>
2015-10-17 00:23:04 +0900

    add test.

--- src/Ushios/Shape/Rectangle.php
+++ src/Ushios/Shape/Rectangle.php
@@ -13,4 +13,36 @@ class Rectangle extends ShapeAbstract implements RectangleInterface
 
     /** @var int $height Rectangle's height */
     protected $height;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
 }
---
+++ tests/Ushios/Shape/RectangleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Ushios\Shape\Test;
+
+use Ushios\Shape\Rectangle;
+
+class RectangleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMakeInstance()
+    {
+        $rectangle = new Rectangle();
+    }
+}
