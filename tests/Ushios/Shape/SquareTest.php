7e3076924ebad0761cc017844920f5363e932f0d
UshioShugo <ushio.s@gmail.com>
2015-10-17 20:44:34 +0900

    add square setter and getter test.

--- tests/Ushios/Shape/SquareTest.php
+++ tests/Ushios/Shape/SquareTest.php
@@ -13,8 +13,11 @@ class SquareTest extends \PHPUnit_Framework_TestCase
     public function testGetterSetter()
     {
         $square = new Square();
-        $square->setWidth(100);
 
+        $square->setWidth(100);
         $this->assertEquals(100, $square->getHeight());
+
+        $square->setHeight(200);
+        $this->assertEquals(200, $square->getWidth());
     }
 }
